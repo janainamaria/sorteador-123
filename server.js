841c7cf0b89fc95a064342d5016ae75d50d02ae4
janaina <janainamariamdm@gmail.com>
2022-04-19 21:12:35 -0300

    fim

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app =express()
+
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(process.env.PORT || 3000,()=>{
+console.log("SERVER ON")
+})
